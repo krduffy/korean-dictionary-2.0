0238aa40bddd02a0ac710e46a379d4ab9e6397d7
yincade <101445104+krduffy@users.noreply.github.com>
2024-11-29 13:56:17 -0500

    tests for known studied api view

--- backend/users/tests/test_auth_views.py
+++ backend/users/tests/test_auth_views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
@@ -5,127 +6,174 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import User
 
+
 class AuthViewsTest(APITestCase):
 
     def setUp(self):
-      self.main_username = 'testusername'
-      self.main_password = 'testpassword'
-      self.user = User.objects.create_user(username=self.main_username, password=self.main_password)
+        self.main_username = "testusername"
+        self.main_password = "testpassword"
+        self.user = User.objects.create_user(
+            username=self.main_username, password=self.main_password
+        )
 
-      self.login_url = reverse('login')
-      self.register_url = reverse('register')
-      self.refresh_url = reverse('refresh')
-      self.change_password_url = reverse('change_password')
-      self.access_token = str(RefreshToken.for_user(self.user).access_token)
+        self.login_url = reverse("login")
+        self.register_url = reverse("register")
+        self.refresh_url = reverse("refresh")
+        self.change_password_url = reverse("change_password")
+        self.access_token = str(RefreshToken.for_user(self.user).access_token)
 
-    def access_cookie_returned(self, response):
-      return 'access' in response.data
+    def access_cookie_returned(self, response: JsonResponse):
+        return "access" in response.json()
 
     def refresh_cookie_set(self, response):
-      return 'refresh' in response.cookies
+        return "refresh" in response.cookies
 
     def test_register_user(self):
 
-      registration_attempts = [
-        # correct usage
-        {'username': 'unused', 'password1': 'validpassword',
-         'password2': 'validpassword', 'expected_status': status.HTTP_200_OK},
-        # username already in use
-        {'username': 'testusername', 'password1': 'validpassword',
-         'password2': 'validpassword', 'expected_status': status.HTTP_400_BAD_REQUEST},
-        # password is too short
-        {'username': 'unused', 'password1': 'short',
-         'password2': 'short', 'expected_status': status.HTTP_400_BAD_REQUEST},
-      ]
+        registration_attempts = [
+            # correct usage
+            {
+                "username": "unused",
+                "password1": "validpassword",
+                "password2": "validpassword",
+                "expected_status": status.HTTP_200_OK,
+            },
+            # username already in use
+            {
+                "username": "testusername",
+                "password1": "validpassword",
+                "password2": "validpassword",
+                "expected_status": status.HTTP_400_BAD_REQUEST,
+            },
+            # password is too short
+            {
+                "username": "unused",
+                "password1": "short",
+                "password2": "short",
+                "expected_status": status.HTTP_400_BAD_REQUEST,
+            },
+        ]
 
-      for attempt in registration_attempts:
-        with self.subTest(case=attempt):
-          num_users_before = len(User.objects.all())
-          response = self.client.post(self.register_url, attempt)
-          num_users_after = len(User.objects.all())
+        for attempt in registration_attempts:
+            with self.subTest(case=attempt):
+                num_users_before = len(User.objects.all())
+                response = self.client.post(self.register_url, attempt)
+                num_users_after = len(User.objects.all())
 
-          self.assertEqual(response.status_code, attempt['expected_status'])
-          
-          if attempt['expected_status'] == status.HTTP_200_OK:
-            self.assertEqual(num_users_after, 1 + num_users_before)
-            self.assertTrue(User.objects.filter(username=attempt['username']).exists())
-            self.assertTrue(self.access_cookie_returned(response))
-            self.assertTrue(self.refresh_cookie_set(response))
-          else:
-            self.assertEqual(num_users_after, num_users_before)
-            self.assertFalse(self.access_cookie_returned(response))
-            self.assertFalse(self.refresh_cookie_set(response))
+                self.assertEqual(response.status_code, attempt["expected_status"])
+
+                if attempt["expected_status"] == status.HTTP_200_OK:
+                    self.assertEqual(num_users_after, 1 + num_users_before)
+                    self.assertTrue(
+                        User.objects.filter(username=attempt["username"]).exists()
+                    )
+                    self.assertTrue(self.access_cookie_returned(response))
+                    self.assertTrue(self.refresh_cookie_set(response))
+                else:
+                    self.assertEqual(num_users_after, num_users_before)
+                    self.assertFalse(self.access_cookie_returned(response))
+                    self.assertFalse(self.refresh_cookie_set(response))
 
     def test_login(self):
-      login_attempts = [
-        # correct credentials
-        {'username': 'testusername', 'password': 'testpassword', 
-          'expected_status': status.HTTP_200_OK},
-        # username correct, password incorrect
-        {'username': 'testusername', 'password': 'wrongpassword', 
-          'expected_status': status.HTTP_401_UNAUTHORIZED},
-        # username incorrect, password correct
-        {'username': 'wrongusername', 'password': 'testpassword', 
-          'expected_status': status.HTTP_401_UNAUTHORIZED},
-        # username missing
-        {'password': 'whynousername', 
-          'expected_status': status.HTTP_400_BAD_REQUEST},
-        # password missing
-        {'username': 'whynopassword', 
-          'expected_status': status.HTTP_400_BAD_REQUEST},
-        # username blank
-        {'username': '', 'password': 'testpassword', 
-          'expected_status': status.HTTP_400_BAD_REQUEST},
-        # password blank
-        {'username': 'testusername', 'password': '', 
-          'expected_status': status.HTTP_400_BAD_REQUEST},
-      ]
-      
-      for attempt in login_attempts:
-        with self.subTest(case=attempt):
-          response = self.client.post(self.login_url, attempt)
-          
-          self.assertEqual(response.status_code, attempt['expected_status'])
-          if attempt['expected_status'] == status.HTTP_200_OK:
-            self.assertTrue(self.access_cookie_returned(response))
-            self.assertTrue(self.refresh_cookie_set(response))
-          else:
-            self.assertFalse(self.access_cookie_returned(response))
-            self.assertFalse(self.refresh_cookie_set(response))
+        login_attempts = [
+            # correct credentials
+            {
+                "username": "testusername",
+                "password": "testpassword",
+                "expected_status": status.HTTP_200_OK,
+            },
+            # username correct, password incorrect
+            {
+                "username": "testusername",
+                "password": "wrongpassword",
+                "expected_status": status.HTTP_401_UNAUTHORIZED,
+            },
+            # username incorrect, password correct
+            {
+                "username": "wrongusername",
+                "password": "testpassword",
+                "expected_status": status.HTTP_401_UNAUTHORIZED,
+            },
+            # username missing
+            {
+                "password": "whynousername",
+                "expected_status": status.HTTP_400_BAD_REQUEST,
+            },
+            # password missing
+            {
+                "username": "whynopassword",
+                "expected_status": status.HTTP_400_BAD_REQUEST,
+            },
+            # username blank
+            {
+                "username": "",
+                "password": "testpassword",
+                "expected_status": status.HTTP_400_BAD_REQUEST,
+            },
+            # password blank
+            {
+                "username": "testusername",
+                "password": "",
+                "expected_status": status.HTTP_400_BAD_REQUEST,
+            },
+        ]
+
+        for attempt in login_attempts:
+            with self.subTest(case=attempt):
+                response = self.client.post(self.login_url, attempt)
+
+                self.assertEqual(response.status_code, attempt["expected_status"])
+                if attempt["expected_status"] == status.HTTP_200_OK:
+                    self.assertTrue(self.access_cookie_returned(response))
+                    self.assertTrue(self.refresh_cookie_set(response))
+                else:
+                    self.assertFalse(self.access_cookie_returned(response))
+                    self.assertFalse(self.refresh_cookie_set(response))
 
     def test_refresh_access_token(self):
-      self.client.cookies.load({'refresh': str(RefreshToken.for_user(self.user))})
-      response = self.client.post(self.refresh_url)
-      self.assertEqual(response.status_code, status.HTTP_200_OK)
-      self.assertTrue(self.access_cookie_returned(response))
+        self.client.cookies.load({"refresh": str(RefreshToken.for_user(self.user))})
+        response = self.client.post(self.refresh_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(self.access_cookie_returned(response))
 
     def test_refresh_token_missing(self):
-      response = self.client.post(self.refresh_url)
-      self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-      self.assertEqual(response.data['detail'], 'A refresh token was not sent.')
-      self.assertFalse(self.access_cookie_returned(response))
+        response = self.client.post(self.refresh_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data["detail"], "A refresh token was not sent.")
+        self.assertFalse(self.access_cookie_returned(response))
 
     def test_change_password(self):
-      self.client.force_authenticate(user=self.user)
-      change_attempts = [
-        # correct usage
-        {'old_password': 'testpassword', 'new_password1': 'newpassword123',
-         'new_password2': 'newpassword123', 'expected_status': status.HTTP_200_OK},
-        # provided username incorrect
-        {'old_password': 'wrongpassword', 'new_password1': 'newpassword456',
-         'new_password2': 'newpassword456', 'expected_status': status.HTTP_400_BAD_REQUEST},
-      ]
+        self.client.force_authenticate(user=self.user)
+        change_attempts = [
+            # correct usage
+            {
+                "old_password": "testpassword",
+                "new_password1": "newpassword123",
+                "new_password2": "newpassword123",
+                "expected_status": status.HTTP_200_OK,
+            },
+            # provided username incorrect
+            {
+                "old_password": "wrongpassword",
+                "new_password1": "newpassword456",
+                "new_password2": "newpassword456",
+                "expected_status": status.HTTP_400_BAD_REQUEST,
+            },
+        ]
 
-      for attempt in change_attempts:
-        with self.subTest(case=attempt):
-          response = self.client.post(self.change_password_url, attempt)
-          self.assertEqual(response.status_code, attempt['expected_status'])
-          if attempt['expected_status'] == status.HTTP_200_OK:
-            self.assertTrue(self.user.check_password(attempt['new_password1']))
-            login_response = self.client.post(self.login_url, {
-              'username': self.main_username,
-              'password': attempt['new_password1'],
-            })
-            self.assertEqual(login_response.status_code, status.HTTP_200_OK)
-          else:
-            self.assertFalse(self.user.check_password(attempt['new_password1']))
+        for attempt in change_attempts:
+            with self.subTest(case=attempt):
+                response = self.client.post(self.change_password_url, attempt)
+                self.assertEqual(response.status_code, attempt["expected_status"])
+                if attempt["expected_status"] == status.HTTP_200_OK:
+                    self.assertTrue(self.user.check_password(attempt["new_password1"]))
+                    login_response = self.client.post(
+                        self.login_url,
+                        {
+                            "username": self.main_username,
+                            "password": attempt["new_password1"],
+                        },
+                    )
+                    self.assertEqual(login_response.status_code, status.HTTP_200_OK)
+                else:
+                    self.assertFalse(self.user.check_password(attempt["new_password1"]))
---
+++ backend/users/tests/test_known_unknown.py
@@ -0,0 +1,176 @@
+from words.tests.fixtures.db_data_mixin import DbDataMixin
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework.status import HTTP_200_OK
+
+
+class KnownUnknownTests(DbDataMixin, APITestCase):
+    """Tests for setting words and hanja characters as known or unknown."""
+
+    def setUp(self):
+        self.tested_word_pk = 517
+        self.tested_hanja_pk = "金"
+
+    def construct_url_with_pk(self, pk):
+        return reverse("update_known_studied", kwargs={"pk": pk})
+
+    def send_request_then_check(
+        self,
+        user,
+        korean_or_hanja,
+        known_or_studied,
+        set_true_or_false,
+        response_status,
+    ):
+        self.client.force_authenticate(user=user)
+        test_pk = (
+            self.tested_word_pk if korean_or_hanja == "korean" else self.tested_hanja_pk
+        )
+        url = self.construct_url_with_pk(test_pk)
+
+        response = self.client.put(
+            url,
+            {
+                "korean_or_hanja": korean_or_hanja,
+                "known_or_studied": known_or_studied,
+                "set_true_or_false": set_true_or_false,
+            },
+        )
+
+        self.assertEqual(response.status_code, response_status)
+        self.regular_user.refresh_from_db()
+
+        if response.status_code == HTTP_200_OK:
+            assertion_func = (
+                self.assertIn if set_true_or_false == "true" else self.assertNotIn
+            )
+            user_model_field = None
+            if korean_or_hanja == "korean":
+                user_model_field = (
+                    user.known_words
+                    if known_or_studied == "known"
+                    else user.studied_words
+                )
+            elif korean_or_hanja == "hanja":
+                user_model_field = (
+                    user.known_hanja
+                    if known_or_studied == "known"
+                    else user.studied_hanja
+                )
+            user_model_field_pks = user_model_field.values_list("pk", flat=True)
+
+            assertion_func(test_pk, user_model_field_pks)
+
+    def verify_counts(self, user, counts):
+        if len(counts) != 4:
+            self.fail("The length of counts must be 4")
+
+        self.assertEqual(self.regular_user.known_words.count(), counts[0])
+        self.assertEqual(self.regular_user.studied_words.count(), counts[1])
+        self.assertEqual(self.regular_user.known_hanja.count(), counts[2])
+        self.assertEqual(self.regular_user.studied_hanja.count(), counts[3])
+
+    def test_setting_word_known(self):
+        user = self.regular_user
+
+        self.send_request_then_check(user, "korean", "known", "true", HTTP_200_OK)
+
+        self.verify_counts(user, [1, 0, 0, 0])
+
+    def test_setting_word_known_idempotent(self):
+        user = self.regular_user
+
+        self.verify_counts(user, [0, 0, 0, 0])
+
+        self.send_request_then_check(user, "korean", "known", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 0, 0, 0])
+
+        # Do the same thing again .
+        self.send_request_then_check(user, "korean", "known", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 0, 0, 0])
+
+    def test_setting_word_known_then_unknown(self):
+        user = self.regular_user
+
+        self.verify_counts(user, [0, 0, 0, 0])
+
+        self.send_request_then_check(user, "korean", "known", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 0, 0, 0])
+
+        # Do the same thing again .
+        self.send_request_then_check(user, "korean", "known", "false", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 0, 0])
+
+    def test_setting_word_unknown_when_already_unknown(self):
+        user = self.regular_user
+
+        self.verify_counts(user, [0, 0, 0, 0])
+
+        # Do the same thing again .
+        self.send_request_then_check(user, "korean", "known", "false", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 0, 0])
+
+    def test_setting_word_studied(self):
+        user = self.regular_user
+
+        self.send_request_then_check(user, "korean", "studied", "true", HTTP_200_OK)
+        self.verify_counts(user, [0, 1, 0, 0])
+
+    def test_setting_word_known_then_studied(self):
+        user = self.regular_user
+
+        self.send_request_then_check(user, "korean", "known", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 0, 0, 0])
+        self.send_request_then_check(user, "korean", "studied", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 1, 0, 0])
+
+    def test_setting_word_not_studied_when_already_not_studied(self):
+        user = self.regular_user
+
+        self.send_request_then_check(user, "korean", "studied", "false", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 0, 0])
+
+    def test_setting_hanja_known(self):
+        user = self.regular_user
+
+        self.send_request_then_check(user, "hanja", "known", "true", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 1, 0])
+
+    def test_setting_hanja_studied(self):
+        user = self.regular_user
+
+        self.send_request_then_check(user, "hanja", "studied", "true", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 0, 1])
+
+    def test_setting_hanja_known_and_studied_then_not_known(self):
+        user = self.regular_user
+
+        self.send_request_then_check(user, "hanja", "known", "true", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 1, 0])
+
+        self.send_request_then_check(user, "hanja", "studied", "true", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 1, 1])
+
+        self.send_request_then_check(user, "hanja", "known", "false", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 0, 1])
+
+    def test_setting_both_known_and_studied_then_undone(self):
+        user = self.regular_user
+
+        self.send_request_then_check(user, "korean", "known", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 0, 0, 0])
+        self.send_request_then_check(user, "korean", "studied", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 1, 0, 0])
+        self.send_request_then_check(user, "hanja", "known", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 1, 1, 0])
+        self.send_request_then_check(user, "hanja", "studied", "true", HTTP_200_OK)
+        self.verify_counts(user, [1, 1, 1, 1])
+
+        self.send_request_then_check(user, "hanja", "studied", "false", HTTP_200_OK)
+        self.verify_counts(user, [1, 1, 1, 0])
+        self.send_request_then_check(user, "korean", "studied", "false", HTTP_200_OK)
+        self.verify_counts(user, [1, 0, 1, 0])
+        self.send_request_then_check(user, "korean", "known", "false", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 1, 0])
+        self.send_request_then_check(user, "hanja", "known", "false", HTTP_200_OK)
+        self.verify_counts(user, [0, 0, 0, 0])
