8476de0b911b535e0db50437c402e795bf71e6d7
yincade <101445104+krduffy@users.noreply.github.com>
2025-01-15 20:31:56 -0500

    updated login form appearance

--- frontend/apps/web/src/web-components/forms/display/LoginForm.tsx
+++ frontend/apps/web/src/web-components/forms/display/LoginForm.tsx
@@ -3,6 +3,9 @@ import { useCallAPIWeb } from "../../../shared-web-hooks/useCallAPIWeb";
 import { useLoginForm } from "@repo/shared/hooks/auth/useLoginForm";
 import { FormResultInfoArea } from "../FormResultInfoArea";
 import { LoginSuccessComponent } from "../success-components/LoginSuccessComponent";
+import { Eye, EyeOff } from "lucide-react";
+import { ReactNode, useState } from "react";
+import { Button } from "../../ui/Button";
 
 export const LoginForm = () => {
   const { requestState, formData, postForm, updateField } = useLoginForm({
@@ -11,13 +14,16 @@ export const LoginForm = () => {
   });
 
   return (
-    <form onSubmit={postForm}>
+    <form
+      className="flex flex-col justify-center items-center"
+      onSubmit={postForm}
+    >
       <InputBoxes
         username={String(formData.username)}
         password={String(formData.password)}
         updateField={updateField}
       />
-      <button type="submit">Login</button>
+      <LoginButton />
       <FormResultInfoArea
         requestState={requestState}
         SuccessComponent={LoginSuccessComponent}
@@ -36,29 +42,143 @@ const InputBoxes = ({
   updateField: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
   return (
-    <>
-      <div>
-        <label htmlFor="username">아이디:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={username}
-          onChange={updateField}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">비밀번호:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={updateField}
-          required
-        />
-      </div>
-    </>
+    <div className="flex flex-col gap-12 justify-center items-center w-[50%] min-w-64 max-w-96">
+      <InputWithLabel formDataName="username" displayedLabel="아이디">
+        <UsernameInput value={username} updateField={updateField} />
+      </InputWithLabel>
+      <InputWithLabel formDataName="password" displayedLabel="비밀번호">
+        <PasswordInput value={password} updateField={updateField} />
+      </InputWithLabel>
+    </div>
   );
+};
+
+const InputWithLabel = ({
+  formDataName,
+  displayedLabel,
+  children,
+}: {
+  formDataName: string;
+  displayedLabel: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      <label className="text-nowrap" htmlFor={formDataName}>
+        {displayedLabel}:
+      </label>
+      {children}
+    </div>
+  );
+};
+
+const UsernameInput = ({
+  value,
+  updateField,
+}: {
+  value: string;
+  updateField: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => {
+  return (
+    <InputBox
+      value={value}
+      inputType="text"
+      formDataName="username"
+      addAdditionalRightPadding={false}
+      updateField={updateField}
+    />
+  );
+};
+
+const PasswordInput = ({
+  value,
+  updateField,
+}: {
+  value: string;
+  updateField: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => {
+  const [inputType, setInputType] = useState<"password" | "text">("password");
+
+  return (
+    <div
+      className="relative h-full w-full rounded-lg 
+                    bg-[color:--background-secondary]
+                    flex flex-row gap-4 min-h-8 items-center"
+    >
+      <div className="flex-1">
+        <InputBox
+          value={value}
+          inputType={inputType}
+          formDataName="password"
+          addAdditionalRightPadding={true}
+          updateField={updateField}
+        />
+      </div>
+      <div className="absolute right-4 z-50 flex-none h-full flex flex-col justify-center items-center">
+        <PasswordToggleVisibilityButton
+          inputType={inputType}
+          setInputType={setInputType}
+        />
+      </div>
+    </div>
+  );
+};
+
+const PasswordToggleVisibilityButton = ({
+  inputType,
+  setInputType,
+}: {
+  inputType: "password" | "text";
+  setInputType: (newInputType: "password" | "text") => void;
+}) => {
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    /* to prevent submission of form on click. */
+    e.preventDefault();
+
+    if (inputType === "password") setInputType("text");
+    else setInputType("password");
+  };
+
+  return (
+    <button onClick={onClick}>
+      {inputType === "password" ? <Eye /> : <EyeOff />}
+    </button>
+  );
+};
+
+const InputBox = ({
+  value,
+  inputType,
+  formDataName,
+  addAdditionalRightPadding,
+  updateField,
+}: {
+  value: string;
+  inputType: string;
+  formDataName: string;
+  addAdditionalRightPadding: boolean;
+  updateField: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => {
+  return (
+    <input
+      className={`h-full w-full ${addAdditionalRightPadding ? "pr-12" : "pr-4"} px-4 py-2 
+                bg-[color:--neutral-color-not-hovering]
+                border border-[color:--border-color]
+                rounded-full
+                outline-none text-[color:--text-primary]
+                focus:ring-2 focus:border-[color:--focus-blue]
+                hover:bg-[color:--neutral-color-hovering]
+                transition-all duration-200`}
+      type={inputType}
+      name={formDataName}
+      id={formDataName}
+      value={value}
+      onChange={updateField}
+      required
+    />
+  );
+};
+
+const LoginButton = () => {
+  return <Button type="submit">로그인</Button>;
 };
--- frontend/apps/web/src/web-components/pages/login-page/LoginPage.tsx
+++ frontend/apps/web/src/web-components/pages/login-page/LoginPage.tsx
@@ -1,10 +1,17 @@
 import { PageWithNavBar } from "../navbar/PageWithNavBar";
 import { LoginForm } from "../../forms/display/LoginForm";
+import { TitledPageContent } from "../TitledPageContent";
 
 export const LoginPage = () => {
   return (
     <PageWithNavBar>
-      <LoginForm />
+      <div className="h-full w-full flex items-center justify-center">
+        <div className="h-[90%] w-[80%] p-6">
+          <TitledPageContent title="로그인">
+            <LoginForm />
+          </TitledPageContent>
+        </div>
+      </div>
     </PageWithNavBar>
   );
 };
