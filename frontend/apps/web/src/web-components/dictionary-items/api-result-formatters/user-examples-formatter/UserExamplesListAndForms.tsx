2ca6e51a3cad32af173be6afa20c46211e6f2c76
yincade <101445104+krduffy@users.noreply.github.com>
2025-02-21 17:33:36 -0500

    added more components to list user example

--- frontend/apps/web/src/web-components/dictionary-items/api-result-formatters/user-examples-formatter/UserExamplesListAndForms.tsx
+++ frontend/apps/web/src/web-components/dictionary-items/api-result-formatters/user-examples-formatter/UserExamplesListAndForms.tsx
@@ -5,6 +5,8 @@ import {
   UserVideoExampleType,
 } from "@repo/shared/types/views/dictionary-items/userExampleItems";
 import { useUserExamplesContext } from "../../api-fetchers/user-examples/UserExamplesContextProvider";
+import { ReactNode } from "react";
+import { Button } from "../../../ui/Button";
 
 export const UserExamplesListAndForms = <
   DataType extends
@@ -21,8 +23,6 @@ export const UserExamplesListAndForms = <
       initialData: initialData,
     });
 
-  const { ListedFormComponent } = useUserExamplesContext();
-
   const getIndexAppliedChangeFieldFunc =
     (index: number) =>
     <Field extends keyof DataType>(field: Field, newValue: DataType[Field]) =>
@@ -31,14 +31,68 @@ export const UserExamplesListAndForms = <
   return (
     <div>
       {listOfDataItems.map((dataItem, id) => (
-        <ListedFormComponent
-          // @ts-ignore
-          data={dataItem}
-          // @ts-ignore
-          changeField={getIndexAppliedChangeFieldFunc(id)}
+        <ListedUserExample
           key={id}
+          // @ts-ignore
+          dataItem={dataItem}
+          changeFieldFunction={getIndexAppliedChangeFieldFunc(id)}
+          saveFunction={() => {}}
+          deleteFunction={() => {}}
         />
       ))}
     </div>
+  );
+};
+
+const ListedUserExample = <
+  DataType extends
+    | UserVideoExampleType
+    | UserExampleSentenceType
+    | UserImageExampleType,
+>({
+  dataItem,
+  changeFieldFunction,
+  saveFunction,
+  deleteFunction,
+}: {
+  dataItem: DataType;
+  changeFieldFunction: <Field extends keyof DataType>(
+    field: Field,
+    newValue: DataType[Field]
+  ) => void;
+  saveFunction: () => void;
+  deleteFunction: () => void;
+}) => {
+  const { ListedFormComponent } = useUserExamplesContext();
+
+  return (
+    <UserExampleStyleWrapper>
+      <ListedFormComponent
+        // @ts-ignore
+        data={dataItem}
+        // @ts-ignore
+        changeField={changeFieldFunction}
+      />
+    </UserExampleStyleWrapper>
+  );
+};
+
+const UserExampleStyleWrapper = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+const DeleteItemButton = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <Button type="button" onClick={onClick}>
+      <div>x</div>
+    </Button>
+  );
+};
+
+const SaveItemButton = ({ onClick }: { onClick: () => void }) => {
+  return (
+    <Button type="button" onClick={onClick}>
+      <div>save</div>
+    </Button>
   );
 };
